6ac318154778f62c102fab5ca5f69f5d09316dbb
Sidney Andrews <sidney@opsgility.com>
2015-05-13 16:58:14 -0400

    Initial commit

---
+++ Blynclight/Device.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Enumeration;
+using Windows.Devices.HumanInterfaceDevice;
+using Windows.UI;
+
+namespace BlyncLight
+{
+    public class Device : IDisposable
+    {
+        private readonly Color DEFAULT_COLOR = Colors.Black;
+
+        private readonly Tuple<DeviceInformation, HidDevice> _store;
+        private Color _color;
+
+        public Device(DeviceInformation info, HidDevice device)
+        {
+            _store = new Tuple<DeviceInformation, HidDevice>(info, device);
+            _color = DEFAULT_COLOR;
+        }
+
+        public DeviceInformation DeviceInformation
+        {
+            get { return _store.Item1; }
+        }
+
+        public HidDevice HIDDevice
+        {
+            get { return _store.Item2; }
+        }
+
+        public Color StatusColor
+        {
+            get { return _color; }
+            set
+            {
+                if (_color != value)
+                {
+                    _color = value;
+                    SendColorCommand(_color);
+                }
+            }
+        }
+
+        private void SendColorCommand(Color color)
+        {
+            Byte red = color.R;
+            Byte green = color.G;
+            Byte blue = color.B;
+
+            var commandBuffer = new Byte[9];
+            commandBuffer[0] = 0x00;
+            commandBuffer[1] = red;
+            commandBuffer[2] = blue;
+            commandBuffer[3] = green;
+            commandBuffer[4] = 0; // 0 is stable, 70 is fast blink, 100 is medium blink 
+            commandBuffer[5] = 0x00;
+            commandBuffer[6] = 0x40;
+            commandBuffer[7] = 0x02;
+            commandBuffer[8] = 0xFF; // Did this turn it off? controlCode & 0xFF 
+
+            WriteOutputReport(commandBuffer);
+        }
+
+        private async void WriteOutputReport(Byte[] data)
+        {
+            var report = _store.Item2.CreateOutputReport();
+
+            // Only grab the byte we need            
+            Byte[] bytesToModify = data;
+
+            WindowsRuntimeBufferExtensions.CopyTo(bytesToModify, 0, report.Data, 0, bytesToModify.Length);
+
+            await _store.Item2.SendOutputReportAsync(report);
+        }
+
+        public void Dispose()
+        {
+            _color = DEFAULT_COLOR;
+        }
+    }
+}
---
+++ Blynclight/Manager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.HumanInterfaceDevice;
+using Windows.Storage;
+
+namespace BlyncLight
+{
+    public class Manager : IDisposable
+    {
+        public Device BlyncLight { get; protected set; }
+        
+        public async Task Init()
+        {
+            this.BlyncLight = await GetBlyncLightDevice();
+        }
+
+        private async Task<Device> GetBlyncLightDevice()
+        {
+            HidDevice device = null;
+            DeviceInformation info = null;
+
+            ushort vendorId = 0x0E53;
+            ushort productId = 0x2516;
+            ushort usagePage = 0xFF00;
+            ushort usageId = 0x0001;
+            
+            string selector = HidDevice.GetDeviceSelector(usagePage, usageId, vendorId, productId);
+
+            var devices = await DeviceInformation.FindAllAsync(selector);
+
+            if (devices.Any())
+            {
+                info = devices.Single();
+                device = await HidDevice.FromIdAsync(devices.Single().Id, FileAccessMode.ReadWrite);
+            }
+
+            return new Device(info, device);
+        }
+
+        public void Dispose()
+        {
+            BlyncLight.Dispose();
+        }
+    }
+}
